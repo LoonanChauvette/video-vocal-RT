1f16f64faf5e3bc21e2d3883a8a4a8929002ef2b
Loonan Chauvette <loonan.chauvette@gmail.com>
2023-03-19 15:41:33 -0400

    audio device selection

--- video_vocal_rt/experiment.py
+++ video_vocal_rt/experiment.py
@@ -9,18 +9,23 @@ import tkinter as tk
 from scipy.io.wavfile import write
 from openpyxl import Workbook
 
+from validation import is_dir, str_to_int
+
 class Parameters:
-    def __init__(self, participant_id="", fixation_duration=1000, white_duration=1000,
-                 audio_duration=6, video_dir="VIDEO_FILES", audio_dir="AUDIO_RECORDINGS",
+    def __init__(self, participant_id="", fixation_cross_duration=1000, white_duration=1000,
+                 audio_duration=5, video_dir="VIDEO_FILES", audio_dir="AUDIO_RECORDINGS",
                  data_dir="DATA_FOLDER", sample_rate=44100):
         self.participant_id = participant_id
-        self.fixation_duration = fixation_duration
-        self.white_duration = white_duration
-        self.audio_duration = audio_duration
+        self.cross_dur = fixation_cross_duration
+        self.white_dur = white_duration
+        self.audio_dur = audio_duration
         self.video_dir = video_dir
         self.audio_dir = audio_dir
         self.data_dir = data_dir
         self.sample_rate = sample_rate
+        self.num_samples = int(self.audio_dur*self.sample_rate)
+        self.audio_devices = [(d["index"], d["name"]) for d in sd.query_devices()]
+        self.audio_device = sd.default.device[0]
         self.unique_key = str(time.time())      
 
     def gui_layout(self):
@@ -32,13 +37,15 @@ class Parameters:
                 [sg.Text("Fixation Duration (ms):")],
                 [sg.Text("White Duration (ms):")],
                 [sg.Text("Audio Duration (s):")],
+                [sg.Text("Audio Device:")],
                 ], element_justification="left", pad=(0, 5)),
 
             sg.Column([
                 [sg.InputText(size = (35, 1), key = "-PARTICIPANT_ID-")],
-                [sg.InputText(default_text="1000", key="-FIX-DURATION-", size=(10, 1))],
-                [sg.InputText(default_text="1000", key="-WHITE-DURATION-", size=(10, 1))],
-                [sg.InputText(default_text="6", key="-AUDIO-DURATION-", size=(10, 1))],
+                [sg.InputText(default_text=str(self.cross_dur), key="-CROSS-DUR-", size=(10, 1))],
+                [sg.InputText(default_text=str(self.white_dur), key="-WHITE-DUR-", size=(10, 1))],
+                [sg.InputText(default_text=str(self.audio_dur), key="-AUDIO-DUR-", size=(10, 1))],
+                [sg.DropDown(self.audio_devices, default_value=self.audio_devices[self.audio_device], size=(40, 1), key="-AUDIO-DEV-")],
                 ], element_justification="left", pad=(0, 5))
             ],
 
@@ -50,9 +57,9 @@ class Parameters:
                 ], element_justification="left", pad=(0, 5)),
 
             sg.Column([
-                [sg.InputText(default_text="VIDEO_FILES", key="-VIDEO-DIR-", size=(40, 1)), sg.FolderBrowse()],
-                [sg.InputText(default_text="AUDIO_RECORDINGS", key="-AUDIO-DIR-", size=(40, 1)), sg.FolderBrowse()],
-                [sg.InputText(default_text="DATA_FOLDER", key="-DATA-DIR-", size=(40, 1)), sg.FolderBrowse()],
+                [sg.InputText(default_text=self.video_dir, key="-VIDEO-DIR-", size=(40, 1)), sg.FolderBrowse()],
+                [sg.InputText(default_text=self.audio_dir, key="-AUDIO-DIR-", size=(40, 1)), sg.FolderBrowse()],
+                [sg.InputText(default_text=self.data_dir, key="-DATA-DIR-", size=(40, 1)), sg.FolderBrowse()],
                 ], element_justification="left", pad=(0, 5))
             ],
             
@@ -68,32 +75,24 @@ class Parameters:
         ]
         return layout
     
-    def validate_directory(self, dir_path):
-        if not os.path.isdir(dir_path):
-            raise ValueError(f"{dir_path} is not a valid directory path")
-        return dir_path
 
-    def validate_positive_int(self, value):
-        if not isinstance(value, int):
-            raise ValueError(f"{value} is not a valid integer")
-        if value <= 0:
-            raise ValueError(f"{value} is not a valid positive integer")
-        return value
 
     def set_attributes_values(self, values):
         self.participant_id = str(values["-PARTICIPANT_ID-"])
-        self.fixation_duration = self.validate_positive_int(int(values["-FIX-DURATION-"]))
-        self.white_duration    = self.validate_positive_int(int(values["-WHITE-DURATION-"]))
-        self.audio_duration    = self.validate_positive_int(int(values["-AUDIO-DURATION-"]))
-        self.video_dir = self.validate_directory(values["-VIDEO-DIR-"])
-        self.audio_dir = self.validate_directory(values["-AUDIO-DIR-"])
-        self.data_dir  = self.validate_directory(values["-DATA-DIR-"])
+        self.white_dur = str_to_int(values["-WHITE-DUR-"])
+        self.cross_dur = str_to_int(values["-CROSS-DUR-"])
+        self.audio_dur = str_to_int(values["-AUDIO-DUR-"])
+        self.audio_dev = str_to_int(values["-AUDIO-DEV-"][0])
+        self.video_dir = is_dir(values["-VIDEO-DIR-"])
+        self.audio_dir = is_dir(values["-AUDIO-DIR-"])
+        self.data_dir  = is_dir(values["-DATA-DIR-"])
 
     def reset_attributes_values(self, window):
         window["-PARTICIPANT_ID-"].update(str(self.participant_id))
-        window["-FIX-DURATION-"].update(str(self.fixation_duration))
-        window["-WHITE-DURATION-"].update(str(self.fixation_duration))
-        window["-AUDIO-DURATION-"].update(str(self.audio_duration))
+        window["-CROSS-DUR-"].update(str(self.cross_dur))
+        window["-WHITE-DUR-"].update(str(self.white_dur))
+        window["-AUDIO-DUR-"].update(str(self.audio_dur))
+        window["-AUDIO-DEV-"].update(self.audio_devices[sd.default.device[0]])
         window["-VIDEO-DIR-"].update(str(self.video_dir))
         window["-AUDIO-DIR-"].update(str(self.audio_dir))
         window["-DATA-DIR-"].update(str(self.data_dir))
@@ -158,8 +157,8 @@ def create_fixation_screen():
 
 def run():
     # Create the GUI to enter experiment parameters, returns a parameters object
-    parameters = get_parameters_from_user()
-    video_files = [f for f in os.listdir(parameters.video_dir) if f.endswith('.avi')]
+    params = get_parameters_from_user()
+    video_files = [f for f in os.listdir(params.video_dir) if f.endswith('.avi')]
     random.shuffle(video_files)
 
     # Set up excel file
@@ -177,15 +176,14 @@ def run():
     cv2.waitKey(0)
 
     for i, video_file in enumerate(video_files):
-        video = cv2.VideoCapture(os.path.join(parameters.video_dir, video_file))
+        video = cv2.VideoCapture(os.path.join(params.video_dir, video_file))
         mspf = int(1000 / video.get(cv2.CAP_PROP_FPS))  # ms per frame
 
         # Fixation screen display
         cv2.imshow('main_window', fixation)
-        cv2.waitKey(parameters.fixation_duration)
+        cv2.waitKey(params.cross_dur)
 
-        num_samples = int(parameters.audio_duration*parameters.sample_rate)
-        recording = sd.rec(num_samples, samplerate=parameters.sample_rate, channels=1)
+        recording = sd.rec(params.num_samples, samplerate=params.sample_rate, channels=1)
         
         while True: # Video playing loop
             ret, frame = video.read()
@@ -196,21 +194,21 @@ def run():
             
         # white screen display
         cv2.imshow('main_window', blank)
-        cv2.waitKey(parameters.white_duration)
+        cv2.waitKey(params.white_dur)
 
         sd.wait() # wait for recording to finish
-        audio_file = f"{video_file[:-4]}_{parameters.participant_id}_{parameters.unique_key}.wav"
-        audio_path = os.path.join(parameters.audio_dir, audio_file)
-        write(audio_path, parameters.sample_rate, recording)
+        audio_file = f"{video_file[:-4]}_{params.participant_id}_{params.unique_key}.wav"
+        audio_path = os.path.join(params.audio_dir, audio_file)
+        write(audio_path, params.sample_rate, recording)
 
-        ws.append([parameters.participant_id, i+1, video_file, audio_path])
+        ws.append([params.participant_id, i+1, video_file, audio_path])
 
         video.release()
 
     cv2.destroyAllWindows()
 
-    data_file = f"{parameters.participant_id}_{parameters.unique_key}.xlsx"
-    wb.save(os.path.join(parameters.data_dir, data_file))
+    data_file = f"{params.participant_id}_{params.unique_key}.xlsx"
+    wb.save(os.path.join(params.data_dir, data_file))
 
 if __name__ == "__main__":
     run()
